3c273b2a226bb00287433962382e63c512e56659
Sebastin Santy <sebastinssanty@gmail.com>
2017-08-08 19:32:45 +0530

    Fix tests on prefs

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
-var git = require('../lib/git');
+var prefs = require('../lib/prefs');
 var mocha = require('mocha');
 var expect = require('chai').expect;
 var Preferences = require('preferences');
 hostpref = new Preferences('autolab.host');
 
-describe('git_changeHost', function() {
+describe('prefs_changeHost', function() {
 	var stdin;
 	var prevVal = hostpref.host.host;
 	beforeEach(function() {
@@ -14,13 +14,13 @@ describe('git_changeHost', function() {
 		process.nextTick(function() {
 			stdin.send('http://test.autolab\n');
 		});
-		git.changeHost();
+		prefs.changeHost();
 	});
 	it('tests and changeback', function() {
 		expect(hostpref.host.host).to.equal('http://test.autolab');
 		process.nextTick(function() {
 			stdin.send(prevVal + '\n');
 		});
-		git.changeHost();
+		prefs.changeHost();
 	});
 });
