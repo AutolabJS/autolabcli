09b334730e22445da37177253f3d5120b51f998c
Unknown <unknown@example.com>
2018-01-10 16:14:59 +0530

    Add test directory.

---
+++ test/data/mock-user.js
@@ -0,0 +1,4 @@
+module.exports = {
+  username: 'Sribalaji',
+  password: '123456789'
+}
---
+++ test/test.js
@@ -0,0 +1,6 @@
+var assert = require('assert');
+describe('Mock', function() {
+  it('should assert true to be true', function() {
+      assert.equal(true, true);
+  });
+});
