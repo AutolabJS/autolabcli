cac7f7915fc79a4db972c6be241fab6d9a8c53a9
1AmNegan <muralisribalaji@gmail.com>
2018-03-02 16:21:16 +0530

    Add unit tests and integration test for init command

--- test/lib/model/init.js
+++ test/lib/model/init.js
@@ -1,11 +1,48 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const chalk = require('chalk');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const nock = require('nock');
 
+chai.use(sinonChai);
+chai.should();
+
+const preferenceManager = require('../../../lib/utils/preference-manager');
 const initModel = require('../../../lib/model/init');
 
 chai.use(chaiAsPromised);
 chai.should();
 
-describe('for initModel', () =>{
+describe('for initModel', () => {
+
+  it('should return status code 200 after successful login', async () => {
+    const fakeServer = nock('https://autolab.bits-goa.ac.in')
+                       .post('/api/v4/session?login=testuser3&password=123');
+    fakeServer.reply(200, {
+      ok: true,
+      name: 'test_user3',
+      private_token: 'zxcvbnb'
+    });
+
+    let status = await initModel.authenticate({
+      username: 'testuser3',
+      password: '123'
+    });
+    status.code.should.equal(200);
+    status.name.should.equal('test_user3');
+    preferenceManager.getPrivateToken().should.equal('zxcvbnb');
+  });
+
+  it('should return status code of 401 when invalid login provided', async () => {
+    const fakeServer = nock('https://autolab.bits-goa.ac.in')
+                  .post('/api/v4/session?login=testuser&password=123');
+    fakeServer.reply(401);
+    let status = await initModel.authenticate({
+      username: 'testuser',
+      password: '123'
+    });
+    status.code.should.equal(401);
+  });
+
 });
